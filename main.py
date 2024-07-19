3ea7fbcad495b73cfeb8c090c9f7e533dc9ebef3
blurryface <114793651+bimopraksw@users.noreply.github.com>
2024-07-19 19:03:44 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,123 +1,91 @@
-import os
+from colorama import init, Fore, Style
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 import requests
 import time
-import json
-from colorama import init, Fore, Style
-import random
-from concurrent.futures import ThreadPoolExecutor
-from datetime import datetime, timedelta
-init(autoreset=True)
 
-# Function to get random color
-def get_random_color():
-    colors = [Fore.GREEN, Fore.YELLOW, Fore.BLUE, Fore.MAGENTA, Fore.CYAN]
-    return random.choice(colors)
 
-# Read and parse the query.txt file
 with open('query.txt', 'r') as file:
-    lines = file.readlines()
+    authorizations = [line.strip() for line in file.readlines()]
 
-# Extract authorization data from each line
-authorizations = [line.strip() for line in lines]
+headers = {
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Language': 'en-GB,en-US;q=0.9,en;q=0.8',
+    'Access-Control-Allow-Origin': '*',
+    'Connection': 'keep-alive',
+    'Cache-Control': 'no-cache',
+    'Host': 'tap-tether.org',
+    'Referer': 'https://tap-tether.org/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-origin',
+    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 17_5_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148'
+}
+
+def log_with_timestamp(message):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print(
+        f"{Fore.BLUE + Style.BRIGHT}[ {timestamp} ]{Style.RESET_ALL}"
+        f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+        f"{message}"
+    )
 
 def format_balance(balance):
     value = float(balance) / 1000000
     return "{:.8f}".format(value)
 
-def fetch_user_data(auth, index):
-    headers = {
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en,id-ID;q=0.9,id;q=0.8,en-US;q=0.7',
-        'access-control-allow-origin': '*',
-        'authorization': auth,
-        'cache-control': 'no-cache',
-        'pragma': 'no-cache',
-        'priority': 'u=1, i',
-        'referer': 'https://tap-tether.org/',
-        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Android WebView";v="126"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 13; M2012K11AG Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.134 Mobile Safari/537.36',
-        'x-requested-with': 'org.telegram.messenger.web'
-    }
-    
-    response = requests.get('https://tap-tether.org/server/login', headers=headers)
-    
+def tt_login(token, index):
+    url = 'https://tap-tether.org/server/login'
+    headers.update({
+        'Authorization': token
+    })
+    response = requests.get(url=url, headers=headers)
     if response.status_code == 200:
         data = response.json()['userData']
         first_name = data['firstName']
         balance = format_balance(data['balance'])
-        balance_gold = format_balance(data['balanceGold'])
         remaining_clicks = data['remainingClicks']
-        
         result = (
-            f"{get_random_color()}{first_name}{Style.RESET_ALL} | "
-            f"USDT: {Fore.GREEN}{balance}{Style.RESET_ALL} | "
-            f"Balance: {Fore.YELLOW}{balance_gold}{Style.RESET_ALL} | "
-            f"Remaining Clicks: {get_random_color()}{remaining_clicks}{Style.RESET_ALL}"
+            f"{Fore.CYAN + Style.BRIGHT}[ {first_name} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.GREEN + Style.BRIGHT}[ USDT {balance} ]{Style.RESET_ALL}"
+            f"{Fore.WHITE + Style.BRIGHT} | {Style.RESET_ALL}"
+            f"{Fore.MAGENTA + Style.BRIGHT}[ Remaining Clicks {remaining_clicks} ]{Style.RESET_ALL}"
         )
-        
         return result, data['lastUpdateClicksTime']
     return None, None
 
-def perform_clicks(auth, last_click_time):
-    headers = {
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'en,id-ID;q=0.9,id;q=0.8,en-US;q=0.7',
-        'access-control-allow-origin': '*',
-        'authorization': auth,
-        'cache-control': 'no-cache',
-        'pragma': 'no-cache',
-        'priority': 'u=1, i',
-        'referer': 'https://tap-tether.org/',
-        'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Android WebView";v="126"',
-        'sec-ch-ua-mobile': '?1',
-        'sec-ch-ua-platform': '"Android"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Linux; Android 13; M2012K11AG Build/TKQ1.220829.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/126.0.6478.134 Mobile Safari/537.36',
-        'x-requested-with': 'org.telegram.messenger.web'
-    }
-    
-    url = f'https://tap-tether.org/server/clicks?clicks=1000&lastClickTime={last_click_time}'
-    response = requests.get(url, headers=headers)
-    
+def tt_clicks(token, last_click_time):
+    url = f"https://tap-tether.org/server/clicks?clicks=1000&lastClickTime={last_click_time}"
+    headers.update({
+        'Authorization': token
+    })
+    response = requests.get(url=url, headers=headers)
     return response.status_code == 200
 
-def fetch_and_print_user_data(auth, index):
+def tt_run(token, index):
     while True:
         try:
             time.sleep(1)
-            result, last_click_time = fetch_user_data(auth, index)
-            
-            if result:
-                perform_clicks(auth, last_click_time)
-                return result
-            else:
-                return Fore.RED + f"Failed to fetch data for Akun {index + 1}"
-        
-        except Exception as e:
-            return Fore.RED + f"Error fetching data for Akun {index + 1}: {e}"
+            result, last_click_time = tt_login(token, index)
 
+            if result:
+                tt_clicks(token, last_click_time)
+                return result
+
+        except Exception as e:
+            log_with_timestamp(f"🍓 {Fore.RED + Style.BRIGHT}[ Account {index + 1} ] [ {e} ]")
+            return
 while True:
+    init(autoreset=True)
     results = []
-    
-    # Use ThreadPoolExecutor to make requests concurrently
     with ThreadPoolExecutor(max_workers=len(authorizations)) as executor:
-        futures = [executor.submit(fetch_and_print_user_data, auth, index) for index, auth in enumerate(authorizations)]
+        futures = [executor.submit(tt_run, token, index) for index, token in enumerate(authorizations)]
         for future in futures:
-            result = future.result()  # Wait for all threads to complete
+            result = future.result()
             if result:
                 results.append(result)
-    
+
     if results:
-        # Clear the previous output
-        print("\033c", end="")  # ANSI escape code to clear the screen
-        # Print all results at once
-        print("\n".join(results), end="\r", flush=True)
-    
-    time.sleep(1)  # Adjust sleep time as needed
+        log_with_timestamp("\n".join(results))
+    time.sleep(1)
